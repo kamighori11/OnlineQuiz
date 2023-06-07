6470b07d2cfb103dfb596cb5e1594f12b43fe37e
Kamran Jameel <kamranjameel11@gmail.com>
2023-06-07 17:08:33 +0500

    finished project

---
+++ app/src/main/java/com/example/onlinequiz/dashboard.java
@@ -0,0 +1,16 @@
+package com.example.onlinequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
